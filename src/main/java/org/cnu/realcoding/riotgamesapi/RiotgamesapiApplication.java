fa15018f5c87d64ddff4d5f9fbe487d9f16a73ab
DoDaek <dbxor8960@naver.com>
2019-04-12 10:20:56 +0900

    START Riotgames API Project

---
+++ src/main/java/org/cnu/realcoding/riotgamesapi/RiotgamesapiApplication.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.riotgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotgamesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotgamesapiApplication.class, args);
+	}
+
+}
