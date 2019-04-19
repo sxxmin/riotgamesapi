07f292817ee84511ba858bdd46cea219dcb68c64
DoDaek <dbxor8960@naver.com>
2019-04-19 14:18:57 +0900

    [RAT-6] Create getLeaguePositionDTO method in RiotGamesApiController

--- src/main/java/controller/RiotGamesApiController.java
+++ src/main/java/controller/RiotGamesApiController.java
@@ -1,10 +1,17 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RiotGamesApiController {
     @Autowired
     private LeaguePositionService leaguePositionService;
+
+    @GetMapping("/summoner")
+    public LeaguePositionDTO getLeaguePositionDTO(@RequestParam String summonerName){
+        return LeaguePositionService.getLeaguePosition(summonerName);
+    }
 }
