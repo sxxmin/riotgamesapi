ee588bc1a91db51bcc1e9bb83cf7fb033d3351b6
DoDaek <dbxor8960@naver.com>
2019-04-19 16:16:44 +0900

    [RAT-14,15] Create LeaguePositionService Class & getLeauePosition method

---
+++ src/main/java/org/cnu/realcoding/riotgamesapi/service/LeaguePositionService.java
@@ -0,0 +1,32 @@
+package org.cnu.realcoding.riotgamesapi.service;
+
+import org.cnu.realcoding.riotgamesapi.domain.LeaguePositionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class LeaguePositionService {
+    @Autowired
+    private RiotGamesApiClient riotGamesApiClient;
+    @Autowired
+    private SummonerService summonerService;
+    @Autowired
+    private CurrentSummonerServiceScoreRepo currentSummonerServiceScoreRepo;
+
+    private List<String> encryptedSummonerIdQueue = new LinkedList<>();
+
+    public LeaguePositionDTO getLeaguePosition(String summonerName){
+        if(){ //todo Is there summonerName in database?
+            //todo Then return database information.
+        } else {
+            String encryptedSummonerId = summonerService.getEncryptedSummonerId(summonerName);
+            LeaguePositionDTO currentLeaguePositionDTO = riotGamesApiClient.getLeaguePositionDTO(encryptedSummonerId);
+            return currentLeaguePositionDTO;
+        }
+    }
+}
+
+//summonerService.getEncryptedSummonerId(summonerName)
