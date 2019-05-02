6c25227932abf558bfe74f29e1fc073e8952544a
rinha7 <eocjstnals12@gmail.com>
2019-05-02 19:42:29 +0900

    Add update Method in repository

--- src/main/java/org/cnu/realcoding/riotgamesapi/repository/CurrentSummonerScoreRepo.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/repository/CurrentSummonerScoreRepo.java
@@ -3,7 +3,10 @@ package org.cnu.realcoding.riotgamesapi.repository;
 import org.cnu.realcoding.riotgamesapi.domain.LeaguePositionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
+
+import javax.management.Query;
 
 @Repository
 public class CurrentSummonerScoreRepo {
@@ -14,5 +17,13 @@ public class CurrentSummonerScoreRepo {
     //Do insert SummonerScore to MongoDB
     void insertCurrentSummonerScore(LeaguePositionDTO leaguePositionDTO) {
         mongoTemplate.insert(leaguePositionDTO);
+    }
+
+    //Update Method in MongoDB
+
+    void updateCurrentSummonerScore(LeaguePositionDTO leaguePositionDTO){
+        Query query = new Query();
+        Criteria criteria = new Criteria();
+
     }
 }
